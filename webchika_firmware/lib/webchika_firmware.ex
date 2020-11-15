56045422b7ed684f1ad793668ed45fc2cec63a7e
Kentaro Kuribayashi <kentarok@gmail.com>
2020-11-15 22:36:43 +0900

    Init project with `mix nerves.new webchika_firmware`

---
+++ webchika_firmware/lib/webchika_firmware.ex
@@ -0,0 +1,18 @@
+defmodule WebchikaFirmware do
+  @moduledoc """
+  Documentation for WebchikaFirmware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> WebchikaFirmware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ webchika_firmware/test/webchika_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule WebchikaFirmwareTest do
+  use ExUnit.Case
+  doctest WebchikaFirmware
+
+  test "greets the world" do
+    assert WebchikaFirmware.hello() == :world
+  end
+end
